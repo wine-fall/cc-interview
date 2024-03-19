2e50d0ae0e236413dc0637ab3011403660328b4e
HappySean <zhaoxiang2845@gmail.com>
2024-03-19 23:09:23 +0800

    feat: init with nextjs

--- src/Carousel.tsx
+++
@@ -1,6 +0,0 @@
-// start coding here...
-function Carousel() {
-  return <div>Carousel</div>;
-}
-
-export default Carousel;
--- src/Wordle.tsx
+++
@@ -1,6 +0,0 @@
-// start coding here...
-function Wordle() {
-  return <div>Wordle</div>;
-}
-
-export default Wordle;
